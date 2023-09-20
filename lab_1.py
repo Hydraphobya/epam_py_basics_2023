5137bbe817f6f0d1e393d292f1efb45ffdd9e13f
Hanna Hlushakova <Hanna.Hlushakova@havi.com>
2023-09-20 11:49:56 +0200

    Lab 5. Create a tool, which will do user generated news feed: 1.User select what data type he wants to add
    2.Provide record type required data
    
    3.Record is published on text file in special format
    
    You need to implement:
    1.News – text and city as input. Date is calculated during publishing.
    2.Privat ad – text and expiration date as input. Day left is calculated during publishing.
    3.Tip of the day. Text and rating as an input.
    Each new record should be added to the end of file. Commit file in git for review.

--- lab_1.py
+++ lab_1.py
@@ -32,12 +32,26 @@ for num in x:
     else:
         odd_counter += 1
         odd_summ += num
-try :
+try:
     print('Even numbers average: ', even_summ / even_counter)
 except ZeroDivisionError:
     print('There is no even numbers in the list!')
-try :
+try:
     print('Odd numbers average: ', odd_summ / odd_counter)
 except ZeroDivisionError:
     print('There is no odd numbers in the list!')
 print('Sorted array: ', x)
+
+"""
+# ---- Another solution ----
+total_sum = sum(random_numbers)
+total_count = len(random_numbers) 
+
+odd_list = [elem for elem in random_numbers if elem % 2]
+odd_sum = sum(odd_list)
+odd_count = len(odd_list) 
+
+even_sum = total_sum - odd_sum
+even_count = total_count - odd_count
+even_avg = even_sum / even_count if even_count else 0 # or None
+"""
